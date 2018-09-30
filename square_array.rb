698043e3b132b73ff1432eb1d55d84502a564436
Brandon Teich <brandonteich123@gmail.com>
2018-09-30 20:11:07 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   length = array.length
   newarray = []
   array.each do |x|
-    newarray = newarray.collect { |x| x**2 }
+    newarray.collect { |x| x**2 }
   end
   newarray
 end
