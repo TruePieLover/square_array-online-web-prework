71022afef0beac8cdfd0cee79d7006622ddf78c4
Brandon Teich <brandonteich123@gmail.com>
2018-09-30 20:08:16 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   length = array.length
   newarray = []
   array.each do |x|
-   newarray = array.inject([]){|x| x << x*x}
+   newarray = array.inject([]){|x| x*x}
   end
   newarray
 end
